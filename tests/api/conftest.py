4cb28369361d0588797baf17297286681c19007c
kris-zvereva <krs.zvrv@gmail.com>
2025-12-17 02:39:25 +0100

    minor

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+from selenium.common import NoSuchElementException, TimeoutException
 
 sys.path.insert(0, str(Path(__file__).parent.parent.parent))
 
--- utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .schema_generator import generate_flexible_schema, generate_schema_from_response
-
-__all__ = ['generate_flexible_schema', 'generate_schema_from_response']
