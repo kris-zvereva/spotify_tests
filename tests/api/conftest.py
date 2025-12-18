82853a4fdde9bcfe0fd85a7ac67a7985b2296765
kris-zvereva <krs.zvrv@gmail.com>
2025-12-18 02:07:14 +0100

    for headless run in jenkins

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -70,6 +70,16 @@ def setup_browser():
     browser.config.timeout = settings.timeout
     browser.config.driver_options = webdriver.ChromeOptions()
 
+    # Chrome options для Jenkins (headless)
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless=new')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-blink-features=AutomationControlled')
+
+    browser.config.driver_options = options
+
     yield
 
     browser.quit()
